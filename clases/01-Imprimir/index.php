cccb77272146e1379f82722bde28afb5eaddd074
mikeGom <Miguel@DESKTOP-J9EUTRP>
2020-04-27 09:03:37 -0500

    1st commmit: Creación del repositorio

---
+++ clases/01-Imprimir/index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <META charset = "utf-8"/>
+        <title>Vendo perros callejeros</title>
+    </head>
+    <body>
+        <!--Comentario-->
+        <h1>Clases Básicas de PHP&HTML</h1>
+        <?php
+        // Comentario
+        echo '<h2>Categoría de Zapatos</h2>';
+        echo "<ul>"
+        . "<li>Dama</li>"
+        . "<li>Caballero</li>"
+        . "<li>Niñes</li>";
+        ?>                
+    </body>
+</html>               
---
+++ clases/02-variables/index.php
@@ -0,0 +1,13 @@
+<!--HTML no puedo guardar un espacio para variables-->
+<?php
+$var1 = "Andy es una chica muy linda";
+$var2 = "Luis es un chico guapo";
+
+$int = 50;
+$real = 50.5;
+$booleana = true;
+$cadena = "andré llegó tarde";
+//echo $int ."<br>". $real."<br>" . "<br>".$booleana ."<br>". $cadena;
+var_dump($cadena);
+?>
+
