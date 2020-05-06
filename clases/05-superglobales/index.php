3c2b5e59b4231aa7f9e523b201e42252f30cf431
mikeGom <Miguel@DESKTOP-J9EUTRP>
2020-05-06 09:53:34 -0500

    Actualizar repositorio

---
+++ clases/05-superglobales/index.php
@@ -0,0 +1,18 @@
+<?php
+//varibles del servidor 
+echo "<h1>". $_SERVER['SERVER_ADDR']. "</h1>";
+
+echo "<h1>". $_SERVER['SERVER_NAME']. "</h1>";
+
+echo "<h1>". $_SERVER['SERVER_SOFTWARE']. "</h1>";
+
+echo "<h1>". $_SERVER['HTTP_USER_AGENT']. "</h1>";
+
+echo "<h1>". $_SERVER['REMOTE_ADDR']. "</h1>";
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
---
+++ clases/05-superglobales/recibir.php
@@ -0,0 +1,2 @@
+<?php
+echo "<h1>".'Bienvenido'." ".$_GET['nombre']." ".$_GET['apellidos']."</h1>";
